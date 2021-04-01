302bb4065a3021568a86e4ad5f47254dbe84cf1a
Yazandar <yazanadd@gmail.com>
2021-04-01 14:13:38 +0300

    after Javascript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HTML.lab
+
+## This is my html and java script 
---
+++ javasc.js
@@ -0,0 +1,20 @@
+
+
+ var visitorName=' '+ prompt('Please Enter Your Name');
+ 
+ if(visitorName > 0){
+     alert('Please your name not a number')
+ var visitorName=' '+ prompt('Please Enter Your Name');
+        }
+       
+        else{
+          alert('Hello'+visitorName);   
+          
+         }
+     document.write(visitorName);
+ 
+ 
+
+
+
+ 
