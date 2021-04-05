8446e35c69ac8e7bf61d0c5c5656d170823d1390
Yazandar <yazanadd@gmail.com>
2021-04-05 18:01:24 +0300

    fixing pull bug

--- javasc.js
+++ javasc.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
      function alerts(param1) {
        alert(param1);
      } 
@@ -37,46 +36,5 @@
     
     
 
-      
-      
-      
-    
-=======
-    var visitorName=' '+ prompt('Please Enter Your Name');
- 
-    alert('Hello'+visitorName);   
-
-    var age = prompt('how old are you?');
-
-  
-    function myFunction(para1,para2) {
-      document.write('visitor information is ',para1,"|||",para2 ,"years old")
-      
-    }
-    
-    while(age < 18){
-      alert('you have to be over 18');
-      age = prompt('how old are you?');
-    }
-
     
   
-    var favNumber=prompt('Whats your favorite number from 1 to 10?');
->>>>>>> 77ef01023d0230d20e78d37ec3385df56fc401ee
-
-    
-
-    for (i = 0; i < favNumber; i++) {
-      document.write("<img src= 'https://freeiconshop.com/wp-content/uploads/edd/html-flat.png'/>");
-    }
-
-    
-
-<<<<<<< HEAD
-    
-  
-=======
-      myFunction(visitorName, age);
-      
-    
->>>>>>> 77ef01023d0230d20e78d37ec3385df56fc401ee
