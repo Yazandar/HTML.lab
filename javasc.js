c0162cc92d2a7e4ab8523da7d4b61dc38dbb63c8
Yazandar <yazanadd@gmail.com>
2021-04-05 16:43:00 +0300

    designing by external and js

--- javasc.js
+++ javasc.js
@@ -1,20 +1,38 @@
-var visitorName=' '+ prompt('Please Enter Your Name');
- 
- if(visitorName > 0){
-     alert('Please your name not a number')
- var visitorName=' '+ prompt('Please Enter Your Name');
-        }
-       
-        else{
-          alert('Hello'+visitorName);   
-          
-         }
-     
- 
-    prompt('how old are you?');
+
+     function alerts(param1) {
+       alert(param1);
+     } 
+   
+
+    var age = prompt('how old are you?');
     
-    prompt('Whats your favorite color?');
+    while(age < 18 ){
+      alerts("you have to be over 18");
+      age = prompt('how old are you?');
+    }
 
 
-
+    var visitorName=prompt('Please Enter Your Name');
  
+    alerts('Hello'+" "+visitorName);
+
+
+    var favNumber=prompt('Whats your favorite number from 1 to 10?');
+
+
+  
+    for (i = 0; i < favNumber; i++) {
+      document.write("<img src= 'https://freeiconshop.com/wp-content/uploads/edd/html-flat.png'/>");
+    }
+
+    document.write(" ",visitorName," ",age,' years old');
+
+
+      
+      
+      
+    
+
+
+    
+  
