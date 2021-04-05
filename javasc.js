835436327c1abc70815557994ceceb7f45e7f1a3
Yazandar <yazanadd@gmail.com>
2021-04-05 17:49:05 +0300

    designing by external and js

--- javasc.js
+++ javasc.js
@@ -2,31 +2,39 @@
      function alerts(param1) {
        alert(param1);
      } 
-   
-
-    var age = prompt('how old are you?');
     
-    while(age < 18 ){
+     function info() {
+
+      var age = prompt('how old are you?');
+
+      while(age < 18 ){
       alerts("you have to be over 18");
       age = prompt('how old are you?');
-    }
+      }
 
-
-    var visitorName=prompt('Please Enter Your Name');
+      var visitorName=prompt('Please Enter Your Name');
  
-    alerts('Hello'+" "+visitorName);
+      alerts('Hello'+" "+visitorName);
+      console.log(" ",visitorName," ",age,' years old');
 
-
-    var favNumber=prompt('Whats your favorite number from 1 to 10?');
-
-
-  
-    for (i = 0; i < favNumber; i++) {
-      document.write("<img src= 'https://freeiconshop.com/wp-content/uploads/edd/html-flat.png'/>");
     }
 
-    document.write(" ",visitorName," ",age,' years old');
+      function logos(num) {
+      for (i = 0; i < num; i++) {
+      document.write("<img src= 'https://freeiconshop.com/wp-content/uploads/edd/html-flat.png'/>");
+        
+        }
+      }
 
+    
+
+    info();
+    
+    var favNum=prompt('Whats your favorite number from 1 to 10?');
+
+    logos(favNum);
+    
+    
 
       
       
